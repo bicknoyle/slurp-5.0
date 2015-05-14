62e5df74fcbb99dbe7e627f42000458b3370c51c
Nick Boyle <nsboyle@gmail.com>
2015-05-13 21:48:03 -0400

    Revert to vanilla bootstrap & add js section

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>Slurp</title>
 
-	<link href="{{ asset('/css/flatly.min.css') }}" rel="stylesheet">
+	<link href="{{ asset('/css/app.css') }}" rel="stylesheet">
 
 	<!-- Fonts -->
 	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
@@ -65,8 +65,11 @@
 	</div>
 	@show
 
+
 	<!-- Scripts -->
+	@section('js')
 	<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
 	<script src="//cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.1/js/bootstrap.min.js"></script>
+	@show()
 </body>
 </html>
