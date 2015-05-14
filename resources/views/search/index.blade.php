1977e5a2ae0394204105f9cbdb873005f9ec1f32
Nick Boyle <nsboyle@gmail.com>
2015-05-14 16:29:32 -0400

    Improved site flow & results display

--- resources/views/search/index.blade.php
+++ resources/views/search/index.blade.php
@@ -28,8 +28,29 @@
 		<button type="submit" class="btn btn-primary">Create Search</button>
 	{!! Form::close() !!}
 
-	<h2>Current Searches</h2>
+	<h2>Active Searches</h2>
 	@forelse($searches as $search)
+		<!-- Confirm Delete Modal -->
+		<div class="modal fade" id="confirm-delete-{{ $search->id }}" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<h4 class="modal-title">Confirm Action</h4>
+					</div>
+					<div class="modal-body">
+						You are about to permanantly delete <strong>{{ $search->title }}</strong> and all of it's saved results. Are you sure?
+					</div>
+					<div class="modal-footer">
+						{!! Form::open(['route' => ['searches.destroy', $search->id], 'method' => 'delete', 'style' => 'display:inline;']) !!}
+							<button type="submit" class="btn btn-danger">Yes, delete</button>
+						{!! Form::close() !!}
+						<button type="button" data-dismiss="modal" class="btn btn-default">No, cancel</button>
+					</div>
+				</div>
+			</div>
+		</div>
+
+
 		<div class="panel panel-default">
 			<div class="panel-heading">{{ $search->title }}</div>
 			<div class="panel-body">
@@ -41,8 +62,19 @@
 							<small class="text-muted">(created {{ $search->created_at->diffForHumans() }})</small>
 						</p>
 					</div>
-					<div class="col-sm-4 text-right">
-						<a class="btn btn-default" href="{{ route('searches.show', ['searches' => $search->id]) }}">View <i class="fa fa-eye"></i></a>
+					<div class="col-sm-4 clearfix">
+						<div class="dropdown pull-right">
+							<button id="search-menu-{{ $search->id }}" class="btn btn-default" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+								<span class="sr-only">Menu</span>
+								<i class="fa fa-ellipsis-v"></i>
+							</button>
+							<ul class="dropdown-menu" role="menu" aria-labelledby="search-menu-{{ $search->id }}">
+								<li><a href="{{ route('searches.show', ['searches' => $search->id]) }}"><i class="fa fa-line-chart fa-fw"></i> Results</a></li>
+								<li><a href="{{ route('searches.download', ['searches' => $search->id]) }}"><i class="fa fa-download fa-fw"></i> Download</a></li>
+								<li class="divider"></li>
+								<li><a data-toggle="modal" data-target="#confirm-delete-{{ $search->id }}" href><i class="fa fa-trash fa-fw"></i> Delete...</a></li>
+							</ul>
+						</div>
 					</div>
 				</div>
 			</div>
--- resources/views/search/show.blade.php
+++ resources/views/search/show.blade.php
@@ -1,70 +1,44 @@
 @extends('app')
 
 @section('content')
-<!-- Modal -->
-<div class="modal fade" id="confirm-delete" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
-	<div class="modal-dialog">
-		<div class="modal-content">
-			<div class="modal-header">
-				<h4 class="modal-title">Confirm Action</h4>
-			</div>
-			<div class="modal-body">
-				You are about to permanantly delete this search and all saved results. Are you sure?
-			</div>
-			<div class="modal-footer">
-				{!! Form::open(['route' => ['searches.destroy', $search->id], 'method' => 'delete', 'style' => 'display:inline;']) !!}
-					<button type="submit" class="btn btn-danger">Yes, delete</button>
-				{!! Form::close() !!}
-				<button type="button" data-dismiss="modal" class="btn btn-default">No, cancel</button>
-			</div>
-		</div>
-	</div>
+
+<p>
+	<a href="{{ route('searches.index') }}"><i class="fa fa-arrow-left"></i> Return</a>
+</p>
+
+<h2>{{ $search->title }}</h2>
+
+<p class="lead">
+	<strong>Terms:</strong>
+	{{ $search->terms }}
+</p>
+
+<div class="jumbotron">
+	<h2>Wouldn't a graph look nice here?</h2>
+	<br>
+	<br>
+	<br>
+	<br>
 </div>
 
-<h2>Search Details</h2>
-<div class="panel panel-default">
-	<div class="panel-heading">{{ $search->title }}</div>
-	<div class="panel-body">
-		<div class="row">
-			<div class="col-sm-8">
-				<div class="lead">{{ $search->terms }}</div>
-				<p>{{ $search->results()->count() }} results</p>
-				<p>
-					<small class="text-muted">(created {{ $search->created_at->diffForHumans() }})</small>
-				</p>
-			</div>
-		</div>
-		<div class="row">
-			<div class="col-xs-12">
-				<h3>Results by date</h3>
-				<table class="table table-striped">
-					<thead>
-						<tr>
-							<th>Date</th>
-							<th>Count</th>
-						</tr>
-					</thead>
-					<tbody>
-						@foreach($daily_results as $result)
-						<tr>
-							<th scope="row">{{ $result->date }}</th>
-							<td>{{ $result->count }}</td>
-						</tr>
-						@endforeach
-					</tbody>
-				</table>
+<table class="table table-striped table-bordered">
+	<thead>
+		<tr>
+			<th>Date</th>
+			<th>Count</th>
+		</tr>
+	</thead>
+	<tbody>
+		@foreach($daily_results as $result)
+		<tr>
+			<th scope="row">{{ $result->date }}</th>
+			<td>{{ $result->count }}</td>
+		</tr>
+		@endforeach
+	</tbody>
+</table>
 
-				<a class="btn btn-default" href="{{ route('searches.download', ['searches' => $search->id]) }}">Download <i class="fa fa-download"></i></a>
-			</div>
-		</div>
-	</div>
-	<div class="panel-footer">
-		<div class="text-right">
-			<button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#confirm-delete" title="Delete search...">
-				<span class="sr-only">Delete Search</span>
-				<i class="fa fa-trash-o fa-lg"></i>
-			</button>
-		</div>
-	</div>
-</div>
+<p>
+	<a class="btn btn-default" href="{{ route('searches.download', ['searches' => $search->id]) }}"><i class="fa fa-download fa-fw"></i> Download Results</a>
+</p>
 @stop
